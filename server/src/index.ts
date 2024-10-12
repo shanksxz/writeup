97e018b81b9789fc5f7e87284ba30037cf884f02
somya bhatt <somyacu1@gmail.com>
2024-10-12 19:11:29 +0530

    fix: types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import logger from "@/config/logger";
 import { PORT } from "@/config/config";
 import userRoutes from "@/routes/user";
@@ -13,35 +13,33 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-// cors
+
 app.use(
-    cors({
-        origin: ["http://localhost:5173", "https://writeup-xi.vercel.app"],
-        methods: "GET,POST,PUT,DELETE",
-        credentials: true,
-    }),
+  cors({
+    origin: ["http://localhost:5173", "https://writeup-xi.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
 );
 
-//connect to the database
 dbconnect();
 
-//logging middleware
-app.use((req, res, next) => {
-    logger.info(`${req.method} ${req.url}`);
-    next();
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 
 app.use("/api", userRoutes);
 app.use("/api", postRoutes);
 
-app.use((req, res, next) => {
-    next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
