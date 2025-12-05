a97072c14fa7ebd5e5c464cc6e6da3e06f8721ac
Somya Bhatt <somyacu1@gmail.com>
2025-12-05 11:21:18 +0530

    feat: update package manager and enhance TypeScript configuration; add custom types

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,18 +35,17 @@ app.use("/api", commentRouter);
 app.use("/api/posts", postRoutes);
 app.use("/api/category", categoryRoutes);
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`));
+});
+
 app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-
   res.status(statusCode).json({
     error: message,
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
-});
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`));
 });
 
 export { app };
--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -44,3 +44,5 @@ declare global {
     }
   }
 }
+
+export {};
